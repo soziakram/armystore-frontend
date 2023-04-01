09f072591d8fcf56b747b7f645526fc0c4876ebe
Asse <you@example.com>
2023-04-02 01:57:38 +0300

    changing padding

--- src/components/category/CategoryCard.jsx
+++ src/components/category/CategoryCard.jsx
@@ -4,8 +4,8 @@ import ButtonSec from "../Button/ButtonSec";
 
 const CategoryCard = () => {
   return (
-    <section className=" p-8 flex flex-col  md:flex-row flex-wrap h-screen sm:h-auto">
-      <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+    <section className=" p-8 flex flex-col md:flex-row flex-wrap h-screen sm:h-auto">
+      <div class=" pb-10 flex flex-col items-center justify-center w-full max-w-sm mx-auto">
         <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md">
           <img
             src="https://images.unsplash.com/photo-1511989130945-c2b632959395?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
@@ -26,7 +26,7 @@ const CategoryCard = () => {
           </div>
         </div>
       </div>
-      <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+      <div class=" pb-10 flex flex-col items-center justify-center w-full max-w-sm mx-auto">
         <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md">
           <img
             src="https://images.unsplash.com/photo-1629309523207-681b18ce3a9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJteSUyMGJvb3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
@@ -47,7 +47,7 @@ const CategoryCard = () => {
           </div>
         </div>
       </div>
-      <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+      <div class=" pb-10 flex flex-col items-center justify-center w-full max-w-sm mx-auto">
         <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md">
           <img
             //   src="IMG_2358.jpeg"
@@ -59,6 +59,69 @@ const CategoryCard = () => {
         <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
           <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
             uniforms
+          </h3>
+
+          <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+            <span class="font-bold text-gray-800 dark:text-gray-200">
+              ALL PRODUCT
+            </span>
+            <ButtonSec />
+          </div>
+        </div>
+      </div>
+      <div class=" pb-10 flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+        <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md">
+          <img
+            src="https://images.unsplash.com/photo-1629309523207-681b18ce3a9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJteSUyMGJvb3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            alt="category"
+          />
+        </div>
+
+        <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+          <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
+            boots
+          </h3>
+
+          <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+            <span class="font-bold text-gray-800 dark:text-gray-200">
+              ALL PRODUCT
+            </span>
+            <ButtonSec />
+          </div>
+        </div>
+      </div>
+      <div class=" pb-10 flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+        <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md">
+          <img
+            src="https://images.unsplash.com/photo-1629309523207-681b18ce3a9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJteSUyMGJvb3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            alt="category"
+          />
+        </div>
+
+        <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+          <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
+            boots
+          </h3>
+
+          <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+            <span class="font-bold text-gray-800 dark:text-gray-200">
+              ALL PRODUCT
+            </span>
+            <ButtonSec />
+          </div>
+        </div>
+      </div>
+      <div class=" pb-10 flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+        <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md">
+          <img
+            src="https://images.unsplash.com/photo-1629309523207-681b18ce3a9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJteSUyMGJvb3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            alt="category"
+          />
+        </div>
+
+        <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+          <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
+            boots
           </h3>
 
           <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
