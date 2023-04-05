3937a70dee8455458ffd02bd39725fb12998f68d
Asse <you@example.com>
2023-04-06 02:59:37 +0300

    side bar w procuct category2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <aside className="w-full p-6 sm:w-60 dark:bg-gray-900 dark:text-gray-100">
+    <aside className="w-fit p-10 sm:w-60 dark:bg-gray-900 dark:text-gray-100">
       <nav className="space-y-8 text-sm">
-        <div className="space-y-2">
-          <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">
+        <div className="space-y-4">
+          <h2 className="text-sm w-fit font-extrabold tracking-widest uppercase dark:text-gray-400">
             OVERVIEW BY CATEGORIES
           </h2>
           <div className="flex flex-col space-y-1">
-            <a rel="noopener noreferrer" href="#">
+            <a
+              className="hover:text-green-600"
+              rel="noopener noreferrer"
+              href="#"
+            >
               JACKETS
             </a>
-            <a rel="noopener noreferrer" href="#">
-            TACTICAL SHIRT
+            <a
+              className="hover:text-green-600"
+              rel="noopener noreferrer"
+              href="#"
+            >
+              TACTICAL SHIRT
             </a>
-            <a rel="noopener noreferrer" href="#">
-            TACTICAL PANTS
+            <a
+              className="hover:text-green-600"
+              rel="noopener noreferrer"
+              href="#"
+            >
+              TACTICAL PANTS
             </a>
             {/* <a rel="noopener noreferrer" href="#">
               Appearance
@@ -51,11 +63,9 @@ const Sidebar = () => {
             </a>
           </div>
         </div> */}
-       
-     
       </nav>
     </aside>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
--- src/components/category/ProductCategory.jsx
+++ src/components/category/ProductCategory.jsx
@@ -3,10 +3,10 @@ import Sidebar from '../Sidebar'
 import ProductCard from './ProductCard'
 const ProductCategory = () => {
       return (
-        <section className="flex flex-row justify-between">
+        <section className="flex flex-row justify-between space-x-36">
           <Sidebar />
-          <div className="flex flex-wrap">
-            <ProductCard />
+          <div className="flex flex-wrap space-y-6 space-x-2">
+    
             <ProductCard />
             <ProductCard />
             <ProductCard />
